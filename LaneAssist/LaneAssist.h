39ea2d9c891b9c2ee597472113bb369a67077341
Garrit <contact@garritschaap.com>
2015-07-03 15:44:25 +0200

    Initial Commit

---
+++ LaneAssist/LaneAssist.h
@@ -0,0 +1,19 @@
+//
+//  LaneAssist.h
+//  LaneAssist
+//
+//  Created by Garrit Schaap on 03.07.15.
+//  Copyright (c) 2015 Garrit Schaap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LaneAssist.
+FOUNDATION_EXPORT double LaneAssistVersionNumber;
+
+//! Project version string for LaneAssist.
+FOUNDATION_EXPORT const unsigned char LaneAssistVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LaneAssist/PublicHeader.h>
+
+
